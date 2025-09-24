19215250cb2991c7b5ff9158a1bad6542acad87f
Carin Barber <cbarber2289@gmail.com>
2025-09-24 16:15:04 -0500

    -added secret message

--- site.js
+++ site.js
@@ -1,15 +1,18 @@
+const key = "It's a secret to everybody.."
+localStorage.setItem(key, 'You got a blue rupee!')
+
 const hours = new Date().getHours()
-const welcome = document.querySelector('#welcome')
 
 const isMorning = hours >= 4 && hours < 12
 const isAfternoon = hours >= 12 && hours < 17
 const isEvening = hours >= 17 && hours < 4
 
+const welcome = document.querySelector('#welcome')
 
 if(isMorning) {
     welcome.textContent = 'Good Morning!'
 }
-    else if(isAfternoon) {
-        welcome.textContent = 'Good Afternoon!'
-    }
-    else {welcome.textContent = 'Good Evening!'}
+else if(isAfternoon) {
+    welcome.textContent = 'Good Afternoon!'
+}
+else { welcome.textContent = 'Good Evening!'}

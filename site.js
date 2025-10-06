67932389638ee6e3595858f14242bdcf1955d55a
Carin Barber <cbarber2289@gmail.com>
2025-10-06 09:21:26 -0500

    -Added Carousel

--- site.js
+++ site.js
@@ -16,3 +16,40 @@ else if(isAfternoon) {
     welcome.textContent = 'Good Afternoon!'
 }
 else { welcome.textContent = 'Good Evening!'}
+
+
+
+const urls = [
+    'https://i.imgur.com/qA2B4U6.jpeg',
+    'https://i.imgur.com/LVx5V3C.jpeg',
+    'https://i.imgur.com/PxIoRgD.jpeg',
+    'https://i.imgur.com/tdozfh1.jpeg',
+    'https://i.imgur.com/VAOqlk9.jpeg'
+].map(url => { (new Image()).src = url; return url })
+
+const btnNext = document.querySelector('#next')
+const btnPrevious = document.querySelector('#prev')
+
+const images = document.querySelectorAll('#carousel img')
+
+let currentImage = 4 
+const showImages = () => {
+    const offset = currentImage % urls.length
+    images.forEach((image, index) => {
+        const imageIndex = (index + offset + urls.length) % urls.length
+        image.src = urls[imageIndex]
+    })
+}
+showImages()
+
+setInterval(() => {  showImages(), currentImage++
+    // code to run EVERY 5 seconds
+}, 5000)
+
+btnNext.addEventListener('click', () => {
+    currentImage++, 
+    showImages()})
+
+btnPrevious.addEventListener('click', () => {
+    currentImage--,
+    showImages()})
